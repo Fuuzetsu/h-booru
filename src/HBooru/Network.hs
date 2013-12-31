18202031c0b5db6a0b56522fcb06239ee01fe995
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-31 00:08:00 +0000

    Naive network fetching of the default tagged posts

--- src/HBooru/Network.hs
+++ src/HBooru/Network.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE UnicodeSyntax #-}
 -- |
 -- Module      :  HBooru.Network
 -- Copyright   :  (c) Mateusz Kowalczyk 2013
@@ -12,4 +13,18 @@
 -- rather than worrying about hard limits per page set by the sites &c.
 module HBooru.Network where
 
+import Control.Applicative ((<$>))
 import HBooru.Types
+import Network.HTTP.Conduit (simpleHttp)
+import Data.ByteString.Lazy (toStrict)
+import Data.ByteString.UTF8
+
+-- | Given a 'Site', 'DataFormat' and a list of 'Tag's, naively fetch the first
+-- page or so and parse it to the appropriate image type. Both the site and the
+-- format need to together form an instance of 'Postable' and the data format
+-- has to exist in an instance of 'CoerceResponse'.
+fetchTaggedPosts
+  :: (Postable s d, CoerceResponse d r) ⇒ s → d → [Tag] → IO [ImageTy s d]
+fetchTaggedPosts s d ts = do
+  parseResponse s . toResponse d . toString . toStrict
+  <$> simpleHttp (postUrl s d ts)
