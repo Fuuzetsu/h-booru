fe38742f835a9498e25f9ababeb5e9c7f1978065
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-03-04 05:44:07 +0000

    Ichijou vinyl post

--- src/HBooru/Parsers/Ichijou.hs
+++ src/HBooru/Parsers/Ichijou.hs
@@ -3,6 +3,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 -- |
 -- Module      :  HBooru.Parsers.Ichijou
@@ -15,32 +18,85 @@
 -- Module for parsing content from <http://ichijou.org/ Ichijou/vectorbooru>.
 module HBooru.Parsers.Ichijou where
 
-import           Data.List
-import qualified HBooru.Parsers.GenericBooru as G
-import           HBooru.Parsers.GenericBooru.TH (makePost)
-import           HBooru.Types
-import           Language.Haskell.TH.Syntax (mkName)
-import           Text.XML.HXT.Core hiding (mkName)
-
--- | Data type for safebooru posts generated using 'makePost'.
-$(makePost (mkName "IchijouPost"))
+import Data.List
+import Data.Vinyl
+import HBooru.Parsers.FieldParsers
+import HBooru.Types
+import Text.XML.HXT.Core hiding (mkName)
 
 -- | We use this type and its 'Site' instance to distinguish
 -- between various parsers.
 data Ichijou = Ichijou
 
+type IchijouPost = PlainRec
+  '[ "creator_id" ::: Integer
+   , "md5" ::: String
+   , "status" ::: String
+   , "preview_height" ::: Integer
+   , "has_notes" ::: Maybe Bool
+   , "author" ::: String
+   , "source" ::: String
+   , "score" ::: Integer
+   , "file_size" ::: String
+   , "sample_width" ::: Integer
+   , "width" ::: Integer
+   , "file_url" ::: String
+   , "sample_height" ::: Integer
+   , "has_children" ::: Bool
+   , "tags" ::: [Tag]
+   , "change" ::: String
+   , "preview_url" ::: String
+   , "has_comments" ::: Maybe Bool
+   , "id" ::: Integer
+   , "sample_url" ::: String
+   , "rating" ::: Rating
+   , "created_at" ::: String
+   , "preview_width" ::: Integer
+   , "parent_id" ::: Maybe Integer
+   , "height" ::: Integer
+   ]
+
+-- | Parser arrow used for Ichijou.
+parsePost ∷ (Functor (cat XmlTree), ArrowXml cat) ⇒ cat XmlTree IchijouPost
+parsePost = hasName "post"
+  >>> creator_idA
+  <:+> md5A
+  <:+> statusA
+  <:+> preview_heightA
+  <:+> has_notesA
+  <:+> authorA
+  <:+> sourceA
+  <:+> scoreA
+  <:+> file_sizeA
+  <:+> sample_widthA
+  <:+> widthA
+  <:+> file_urlA
+  <:+> sample_heightA
+  <:+> has_childrenA
+  <:+> tagsA
+  <:+> changeA
+  <:+> preview_urlA
+  <:+> has_commentsA
+  <:+> idA
+  <:+> sample_urlA
+  <:+> ratingA
+  <:+> created_atA
+  <:+> preview_widthA
+  <:+> parent_idA
+  <:+> heightA
+
+
 instance Postable Ichijou XML where
   postUrl _ _ ts =
-    let tags = intercalate "+" ts
-    in "http://ichijou.org/post/index.xml?tags=" ++ tags
+    let tags' = intercalate "+" ts
+    in "http://ichijou.org/post/index.xml?tags=" ++ tags'
   hardLimit _ = Limit 100
 
 instance Site Ichijou where
 
 instance PostParser Ichijou XML where
   type ImageTy Ichijou XML = IchijouPost
-  parseResponse _ = map (`betweenPosts` IchijouPost)
-                    . runLA (xreadDoc /> G.parsePost) . getResponse
+  parseResponse _ = runLA (xreadDoc /> parsePost) . getResponse
 
 instance Counted Ichijou XML where
   parseCount _ = read . head . runLA (xreadDoc >>> hasName "posts"
